6d6fd6c668884d973f91ceab87dbadcaac9da397
Ishana <raj.bhuvana@gmail.com>
2025-05-16 23:06:35 +0530

    Update 2 Comparing_numbers.py

--- Comparing_numbers.py
+++ Comparing_numbers.py
@@ -1,5 +1,5 @@
 #Code to find the largest number
-'''max=int(input("Enter maximum numbers to compare: "))
+max=int(input("Enter maximum numbers to compare: "))
 l=list()
 for i in range (max):
     n=int(input("Enter a number: "))
@@ -8,9 +8,10 @@ a=l[0]
 for i in l:
     if a<i:
         a=i
-print (a)'''
+print ("Largest Number is: ",a)
 
-'''max=int(input("Enter maximum numbers to compare: "))
+#Code to count maximum number of even integers
+max=int(input("Enter maximum numbers to compare: "))
 l=list()
 for i in range (max):
     n=int(input("Enter a number: "))
@@ -19,17 +20,19 @@ c=0
 for i in l:
     if i%2==0:
         c=c+1
-print ('The number of even numbers are: ',c)'''
+print ('The number of even numbers are: ',c)
 
-'''l=[1,2,2,4,5,5,9]
+#Code to remove duplicates
+l=[1,2,2,4,5,5,9]
 d=list()
 for i in range (len(l)):
     if l[i] in d:
         continue
     else:
         d.append(l[i])
-print(d)'''
+print("List without duplicates: ",d)
 
+#Code to reverse list
 max=int(input("Enter maximum numbers to compare: "))
 l=list()
 for i in range (max):
@@ -39,5 +42,6 @@ d=list()
 f=len(l)-1
 for i in range (f,-1,-1):
     d.append(l[i])
-print(d)
+print("Reveresed List: ",d)
         
+'''please copy code block carefully'''
